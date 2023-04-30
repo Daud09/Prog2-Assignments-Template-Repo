984acd6727dcce2b822cd68f456afc7f5aceb27f
Ramy Ahmed <114768873+RamyAhmed6115@users.noreply.github.com>
2023-04-30 18:10:49 +0200

    Update and rename assign_03_problem_01_code.txt to assign_03_problem_01_code.java

---
+++ assign_03_problem_01_code.java
@@ -0,0 +1,18 @@
+//Ramy Ahmed
+//20216115
+import java.util.*;
+public class Assignment{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
